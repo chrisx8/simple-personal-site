6f26d931fa13539da2cb19efb2f17325b25e991b
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2018-11-25 20:04:30 -0500

    add bulma css

--- contact/forms.py
+++ contact/forms.py
@@ -3,7 +3,7 @@ from snowpenguin.django.recaptcha3.fields import ReCaptchaField
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, min_length=1)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea, min_length=1)
+    name = forms.CharField(max_length=100, min_length=1, widget=forms.TextInput(attrs={'class': 'input'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'input'}))
+    message = forms.CharField(min_length=1, widget=forms.Textarea(attrs={'class': 'textarea'}))
     captcha = ReCaptchaField()
