939920c3a18b5601e276ad1153b4dbc65c6b3183
chrisx8 <30990835+chrisx8@users.noreply.github.com>
2018-11-30 17:30:02 -0500

    icons and error pages

--- simple_personal_site/urls.py
+++ simple_personal_site/urls.py
@@ -14,14 +14,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import render_to_response
 from django.urls import include, path
+from django.views.generic.base import RedirectView
+from simple_personal_site import settings
 from contact import urls as contact_urls
 from shorturl import urls as shorturl_urls
 from projects import urls as projects_urls
 
 urlpatterns = [
+    # serve favicon
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'icons/favicon.ico')),
     path('admin/', admin.site.urls),
     path('contact/', include(contact_urls)),
     path('go/', include(shorturl_urls)),
     path('projects/', include(projects_urls)),
 ]
+
+
+# 404 error page
+def handler404(request, *args, **argv):
+    response = render_to_response('404.html')
+    response.status_code = 404
+    return response
+
+
+# 500 error page
+def handler500(request, *args, **argv):
+    response = render_to_response('500.html')
+    response.status_code = 500
+    return response
