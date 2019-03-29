6edefeee6343253509e362f63e316890b6c909e8
chrisx8 <30990835+chrisx8@users.noreply.github.com>
2019-03-28 22:10:17 -0400

    lazy load images

--- contact/forms.py
+++ contact/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from snowpenguin.django.recaptcha2.fields import ReCaptchaField
 from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
+from simple_personal_site.site_config import RECAPTCHA_PUBLIC_KEY, RECAPTCHA_PRIVATE_KEY
 
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, min_length=1, widget=forms.TextInput(attrs={'class': 'input'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'input', 'type': 'email'}))
     message = forms.CharField(min_length=1, widget=forms.Textarea(attrs={'class': 'textarea'}))
-    captcha = ReCaptchaField(widget=ReCaptchaWidget())
+    if RECAPTCHA_PUBLIC_KEY and RECAPTCHA_PRIVATE_KEY:
+        captcha = ReCaptchaField(widget=ReCaptchaWidget())
--- simple_personal_site/forms.py
+++ simple_personal_site/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from .site_config import RECAPTCHA_PRIVATE_KEY, RECAPTCHA_PUBLIC_KEY
 from snowpenguin.django.recaptcha2.fields import ReCaptchaField
 from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
 
@@ -7,4 +8,5 @@ from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
 class AuthFormCaptcha(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'input'}))
     password = forms.CharField(min_length=1, widget=forms.PasswordInput(attrs={'class': 'input'}))
-    captcha = ReCaptchaField(widget=ReCaptchaWidget())
+    if RECAPTCHA_PRIVATE_KEY and RECAPTCHA_PUBLIC_KEY:
+        captcha = ReCaptchaField(widget=ReCaptchaWidget())
