2d7cb9cb6b839a827b804ef1f6aadc407d0332d0
chrisx8 <30990835+chrisx8@users.noreply.github.com>
2019-05-11 09:26:52 -0400

    header id

--- global_config/templatetags/markdown.py
+++ global_config/templatetags/markdown.py
@@ -1,27 +1,44 @@
 # from bleach_whitelist import markdown_tags, markdown_attrs
 from bleach import clean
 from django import template
+from django.utils.text import slugify
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
 import mistune
+import re
 
 register = template.Library()
 
 
-class HighlightRenderer(mistune.Renderer):
+class SPSRenderer(mistune.Renderer):
     def block_code(self, code, lang):
         if not lang:
-            return '\n<pre><code>%s</code></pre>\n' % mistune.escape(code)
+            return f'\n<pre><code>{mistune.escape(code)}</code></pre>\n'
         lexer = get_lexer_by_name(lang, stripall=True)
         formatter = HtmlFormatter()
         return highlight(code, lexer, formatter)
+    
+    def header(self, text, level, raw=None):
+        # remove html tags and slugify title
+        HTML_TAG_RE = re.compile(r'<[^>]+>')
+        id_tag = slugify(HTML_TAG_RE.sub('', text))
+        # return header html with id
+        return f'\n<h{level} id="{id_tag}">{text}</h{level}>\n'
+
+    def image(self, src, title, alt_text):
+        if title is not None:
+            title = f'title="{mistune.escape(title)}"'
+        if alt_text is not None:
+            alt_text = f'alt="{mistune.escape(alt_text)}"'
+        # add lazyload tag
+        return f'<img class="lazy" {alt_text} {title} src="{mistune.escape(src)}">'
 
 
 @register.filter
 def markdown(value):
     # parse markdown
-    renderer = HighlightRenderer()
+    renderer = SPSRenderer()
     md = mistune.Markdown(renderer=renderer)
     html = md(value)
     # define allowed html tags and attributes
