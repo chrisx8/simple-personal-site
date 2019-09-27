07c81a990fcf80a07e455712f777795cac0ee319
chrisx8 <30990835+chrisx8@users.noreply.github.com>
2019-09-26 20:28:39 -0400

    dark recaptcha

--- contact/forms.py
+++ contact/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from django.conf import settings
 from snowpenguin.django.recaptcha2.fields import ReCaptchaField
+from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
 
 
 class CaptchaForm(forms.Form):
     if settings.RECAPTCHA_PRIVATE_KEY and settings.RECAPTCHA_PUBLIC_KEY:
-        captcha = ReCaptchaField()
+        captcha = ReCaptchaField(widget=ReCaptchaWidget(theme='dark'))
 
 
 class ContactForm(forms.Form):
@@ -13,4 +14,4 @@ class ContactForm(forms.Form):
     email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'input', 'type': 'email'}))
     message = forms.CharField(min_length=1, widget=forms.Textarea(attrs={'class': 'textarea'}))
     if settings.RECAPTCHA_PRIVATE_KEY and settings.RECAPTCHA_PUBLIC_KEY:
-        captcha = ReCaptchaField()
+        captcha = ReCaptchaField(widget=ReCaptchaWidget(theme='dark'))
