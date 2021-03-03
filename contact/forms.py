9d4d8771759215f6fa6d320d61de5aafe1dd7ce4
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2021-03-03 18:57:08 -0500

    fix contact email

--- contact/forms.py
+++ contact/forms.py
@@ -4,4 +4,5 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, min_length=1, widget=forms.TextInput(attrs={'class': 'input'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'input', 'type': 'email'}))
+    subject = forms.CharField(max_length=300, required=False, widget=forms.TextInput(attrs={'class': 'input'}))
     message = forms.CharField(min_length=1, widget=forms.Textarea(attrs={'class': 'textarea'}))
