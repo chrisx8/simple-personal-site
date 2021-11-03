8d22ce066dd083ae8f2b88b68c8334ee9ae1103e
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2021-11-03 16:48:51 -0400

    update help test for url shortener * warn against using built-in path

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class URL(models.Model):
     alias = models.CharField(primary_key=True, max_length=50, default='', null=False,
-                             help_text='Example: XYZ is the alias in shortened URL example.com/XYZ')
+                             help_text='Example: XYZ is the alias in shortened URL example.com/XYZ<br>' +
+                                       '<strong>Alias can\'t be one of the built-in paths (blog, contact, ' +
+                                       'projects, media, static, favicon.ico, sitemap.xml)')
     full_url = models.URLField(default='', null=False)
 
     class Meta:
